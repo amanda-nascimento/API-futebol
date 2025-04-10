3603c7474fa41f091b64e9ebd2286d92987d0b74
Amanda Nascimento <amandamicaele.ns@gmail.com>
2025-04-10 15:39:58 -0300

    Adiçao do gitignore e ajustes dos testes unitários

--- src/test/java/org/meli/crud/ClubeControllerTest.java
+++ src/test/java/org/meli/crud/ClubeControllerTest.java
@@ -1,14 +1,10 @@
 package org.meli.crud;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.meli.crud.controller.ClubeController;
 import org.meli.crud.dto.ClubeDTO;
-import org.meli.crud.repository.ClubeRepository;
 import org.meli.crud.service.ClubeService;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
